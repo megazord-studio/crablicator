6e7f1c071224a9fdc02382a85cacdcba148d1f47
Matthias Osswald <matthias@busykoala.io>
2024-03-17 13:11:23 +0100

    Subscribe all.

--- src/crablicator/main.rs
+++ src/crablicator/main.rs
@@ -1,11 +1,22 @@
 use tokio;
-use std::error::Error;
 use std::env;
+use eventstore::{Client, ClientSettings, ReadAllOptions, StreamPosition};
+use std::error::Error;
 use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
-    println!("{}", env::var("CONNECTION_STRING")?);
+    let settings = env::var("CONNECTION_STRING")?
+        .parse::<ClientSettings>()?;
+      let client = Client::new(settings)?;
+    let options = ReadAllOptions::default()
+        .position(StreamPosition::Start)
+        .forwards();
+    let mut stream = client.read_all(&options).await?;
+    while let Some(event) = stream.next().await? {
+        let test_event = event.get_original_event();
+        println!("Event> {:?}", test_event);
+    }
     Ok(())
 }

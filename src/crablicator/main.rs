8494c8531cb56f71ca87fc1a6cbd334e4ed5093b
Matthias Osswald <matthias@busykoala.io>
2024-03-17 15:42:42 +0100

    refactor into writer.

--- src/crablicator/main.rs
+++ src/crablicator/main.rs
@@ -1,9 +1,10 @@
 use tokio;
 use std::env;
-use eventstore::{Client, ClientSettings, ReadAllOptions, StreamPosition, ResolvedEvent, AppendToStreamOptions, ExpectedRevision, EventData};
+use eventstore::{Client, ClientSettings, ReadAllOptions, StreamPosition, ResolvedEvent, AppendToStreamOptions, ExpectedRevision, EventData, RecordedEvent};
 use std::error::Error;
 use dotenv::dotenv;
 use tokio::sync::mpsc;
+use serde::{Serialize, Deserialize};
 
 const WORKERS: usize = 10;
 
@@ -21,22 +22,54 @@ async fn read_events(
     Ok(())
 }
 
+#[derive(Serialize, Deserialize)]
+struct TestEvent {
+    pub id: String,
+    pub important_data: String,
+}
+
+async fn writer(
+    client: &Client,
+    original_event: &RecordedEvent,
+) -> Result<(), Box<dyn Error>> {
+    let event_type = original_event.event_type.clone();
+    let event_data = original_event.data.clone();
+    let options = AppendToStreamOptions::default()
+        .expected_revision(ExpectedRevision::Any);
+
+    let new_event: EventData;
+    if event_type == "TestEvent" {
+        let mut test_event = serde_json::from_slice::<TestEvent>(&event_data)?;
+        test_event.important_data = "new data".to_string();
+        new_event = EventData::json(
+            event_type.clone(),
+            test_event,
+        )?;
+    } else {
+        new_event = EventData::binary(
+            event_type.clone(),
+            event_data.clone(),
+        );
+    }
+    if event_type == "TestEvent" {
+        println!("Writing event to stream: {:?}", new_event);
+    }
+    client.append_to_stream(
+        original_event.stream_id.to_string(),
+        &options,
+        new_event).await?;
+    Ok(())
+}
+
 async fn write_events(
     client: &Client,
     rx: &mut mpsc::Receiver<ResolvedEvent>,
 ) -> Result<(), Box<dyn Error>> {
-    let options = AppendToStreamOptions::default()
-        .expected_revision(ExpectedRevision::Any);
     while let Some(event) = rx.recv().await {
         let original_event = event.get_original_event();
-        let new_event = EventData::binary(
-            original_event.event_type.clone(),
-            original_event.data.clone()
-        );
-        client.append_to_stream(
-            event.get_original_event().stream_id.to_string(),
-            &options,
-            new_event).await?;
+        let event_stream_id = original_event.stream_id.clone();
+        // split into different workers based on stream id
+        writer(client, original_event).await?;
     }
     Ok(())
 }

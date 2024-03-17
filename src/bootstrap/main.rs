68273c7003c301fed15e62956f450cf6504a4e30
Matthias Osswald <matthias@busykoala.io>
2024-03-17 11:04:26 +0100

    WIP

--- src/bootstrap/main.rs
+++ src/bootstrap/main.rs
@@ -1,25 +1,46 @@
 use tokio;
-// use uuid::Uuid;
 use std::env;
+use uuid::Uuid;
 use serde::{Serialize, Deserialize};
-//use eventstore::{Client, ClientSettings, EventData};
-// use eventstore::{ClientSettings};
+use eventstore::{ClientSettings, Client, EventData, AppendToStreamOptions, ExpectedRevision};
 use std::error::Error;
 use dotenv::dotenv;
+use rand::seq::SliceRandom;
 
 #[derive(Serialize, Deserialize)]
 struct TestEvent {
-    id: String,
-    important_data: String,
+    pub id: String,
+    pub important_data: String,
+}
+
+async fn send_event(
+    client: &Client, stream: &str
+) -> Result<(), Box<dyn Error>> {
+    let data = TestEvent {
+        id: Uuid::new_v4().to_string(),
+        important_data: "some value".to_string(),
+    };
+    let evt = EventData::json("language-poll", data)?.id(Uuid::new_v4());
+    let options = AppendToStreamOptions::default()
+        .expected_revision(ExpectedRevision::NoStream);
+    client
+        .append_to_stream(stream, &options, evt)
+        .await?;
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
-    println!("{}", env::var("CONNECTION_STRING")?);
-    //let connection_string = env::var("CONNECTION_STRING")?
-        //.parse::<ClientSettings>()?;
-    // println!("{:?}", connection_string);
+    let settings = env::var("CONNECTION_STRING")?
+        .parse::<ClientSettings>()?;
+    let client = Client::new(settings)?;
+
+    let categories = vec!["apple", "banana", "cherry", "date", "elderberry"];
+    for _ in 0..20 {
+        let topic = categories.choose(&mut rand::thread_rng()).expect("Topics list is empty");
+        println!("Sending event to topic {}", topic);
+    }
 
     Ok(())
 }
--- src/crablicator/main.rs
+++ src/crablicator/main.rs
@@ -1,14 +1,7 @@
 use tokio;
-use serde::{Serialize, Deserialize};
 use std::error::Error;
 use std::env;
 use dotenv::dotenv;
-
-#[derive(Serialize, Deserialize)]
-struct TestEvent {
-    id: String,
-    important_data: String,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {

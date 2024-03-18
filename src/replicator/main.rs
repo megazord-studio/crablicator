0ffb008dfb5eb92a9f0d2db67d9028844a9b1c99
Simon Josi <simon@megazord.studio>
2024-03-18 20:25:58 +0100

    make it build again

--- src/replicator/main.rs
+++ src/replicator/main.rs
@@ -1,7 +1,7 @@
 use dotenv::dotenv;
 use eventstore::{
-    AppendToStreamOptions, Client, ClientSettings, EventData, ExpectedRevision, ReadAllOptions,
-    RecordedEvent, ResolvedEvent, StreamPosition,
+    AppendToStreamOptions, Client, EventData, ExpectedRevision, ReadAllOptions, ResolvedEvent,
+    StreamPosition,
 };
 use std::collections::hash_map::DefaultHasher;
 use std::env;
@@ -10,28 +10,27 @@ use std::hash::{Hash, Hasher};
 use tokio;
 use tokio::sync::mpsc;
 
+use crablicator::util::esdb;
 mod transformations;
 
 const WORKERS: usize = 10;
+const CHANNEL_BUFFER_SIZE: usize = 1000000;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
-    let origin_settings = env::var("ORIGIN_CONNECTION_STRING")?.parse::<ClientSettings>()?;
-    let destination_settings =
-        env::var("DESTINATION_CONNECTION_STRING")?.parse::<ClientSettings>()?;
-    let origin_client = Client::new(origin_settings)?;
-    let destination_client = Client::new(destination_settings)?;
-    let (tx, mut rx) = mpsc::channel::<ResolvedEvent>(10000);
+    let reader_client = esdb::get_client(&env::var("REPLICATOR_READER_CONNECTION_STRING")?)?;
+    let writer_client = esdb::get_client(&env::var("REPLICATOR_READER_CONNECTION_STRING")?)?;
+    let (tx, mut rx) = mpsc::channel::<ResolvedEvent>(CHANNEL_BUFFER_SIZE);
     let reader_handle = tokio::spawn(async move {
-        read_events(&origin_client, &tx)
+        read_events(&reader_client, &tx)
             .await
-            .expect("Failed to read event");
+            .expect("Error in read_events");
     });
     let writer_handle = tokio::spawn(async move {
-        write_events(&destination_client, &mut rx)
+        write_events(&writer_client, &mut rx)
             .await
-            .expect("Failed to write event");
+            .expect("Error in write_events");
     });
     futures::future::join_all([reader_handle, writer_handle]).await;
     Ok(())
@@ -46,7 +45,9 @@ async fn read_events(
         .forwards();
     let mut stream = client.read_all(&options).await?;
     while let Some(event) = stream.next().await? {
-        tx.send(event).await.expect("Failed to tx event");
+        tx.send(event)
+            .await
+            .expect("Failed to send event from read_events to write_events");
     }
     Ok(())
 }
@@ -56,17 +57,16 @@ async fn write_events(
     rx: &mut mpsc::Receiver<ResolvedEvent>,
 ) -> Result<(), Box<dyn Error>> {
     let (txs, rxs): (Vec<_>, Vec<_>) = (0..WORKERS)
-        .map(|_| mpsc::channel::<ResolvedEvent>(100))
+        .map(|_| mpsc::channel::<ResolvedEvent>(CHANNEL_BUFFER_SIZE))
         .unzip();
-    for (i, rx) in rxs.into_iter().enumerate() {
+    for rx in rxs.into_iter() {
         let worker_client = client.clone();
         tokio::spawn(async move {
-            worker(i, &worker_client, rx).await;
+            worker(&worker_client, rx).await;
         });
     }
     while let Some(event) = rx.recv().await {
-        let original_event = event.get_original_event();
-        let event_stream_id = original_event.stream_id.clone();
+        let event_stream_id = event.get_original_event().stream_id.clone();
         let mut hasher = DefaultHasher::new();
         event_stream_id.hash(&mut hasher);
         let worker_index = (hasher.finish() as usize) % WORKERS;
@@ -78,20 +78,22 @@ async fn write_events(
     Ok(())
 }
 
-async fn worker(id: usize, client: &Client, mut rx: mpsc::Receiver<ResolvedEvent>) {
+async fn worker(client: &Client, mut rx: mpsc::Receiver<ResolvedEvent>) {
     while let Some(event) = rx.recv().await {
-        let original_event = event.get_original_event();
-        if let Err(e) = writer(&client, original_event).await {
-            eprintln!("Error processing event in worker {}: {}", id, e);
+        if let Err(e) = append_to_stream(&client, &event).await {
+            eprintln!("Error in append_to_stream {}", e);
         }
     }
 }
 
-async fn writer(client: &Client, original_event: &RecordedEvent) -> Result<(), Box<dyn Error>> {
-    let event_type = original_event.event_type.clone();
-    let event_data = original_event.data.clone();
+async fn append_to_stream(client: &Client, event: &ResolvedEvent) -> Result<(), Box<dyn Error>> {
+    let original_event = event.get_original_event();
+    let new_event: EventData = transformations::transform_event(
+        original_event.event_type.clone(),
+        original_event.data.clone(),
+    )
+    .await?;
     let options = AppendToStreamOptions::default().expected_revision(ExpectedRevision::Any);
-    let new_event: EventData = transformations::transform_event(event_type, event_data).await?;
     client
         .append_to_stream(original_event.stream_id.to_string(), &options, new_event)
         .await?;
